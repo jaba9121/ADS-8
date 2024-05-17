3efe5d4bb336ba045f42289aaa597b78d22b5094
jaba9121 <160114001+jaba9121@users.noreply.github.com>
2024-05-17 22:03:08 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -4,17 +4,17 @@
 
 class Train {
  private:
-    struct Cage {
-        bool light;
-        Cage *next;
-        Cage *prev;
-    };
-    int countOp;
-    Cage *first;
+  struct Cage {
+    bool light; // состояние лампочки
+    Cage *next;
+    Cage *prev;
+  };
+  int countOp; // счетчик шагов (число переходов из вагона в вагон)
+  Cage *first; // точка входа в поезд (первый вагон)
  public:
-    Train();
-    void addCage(bool light);
-    int getLength();
-    int getOpCount();
+  Train();
+  void addCage(bool light); // добавить вагон с начальным состоянием лампочки
+  int getLength();          // вычислить длину поезда
+  int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
 #endif  // INCLUDE_TRAIN_H_
