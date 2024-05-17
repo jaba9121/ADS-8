aecd781340e9fda71f4d8e2fc9d1d082ecef0682
jaba9121 <160114001+jaba9121@users.noreply.github.com>
2024-05-17 22:03:42 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,48 +1,55 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : first(nullptr), end(nullptr) {}
+Train::Train() {
+    countOp = 0;
+    first = nullptr;
+}
 
 void Train::addCage(bool light) {
-    if (!first) {
-        first = new Cage;
-        first->light = light;
-        end = first;
-        first->next = first;
-        first->prev = first;
+    Cage* newCage = new Cage();
+    newCage->light = light;
+    if (first == nullptr) {
+        newCage->next = newCage;
+        newCage->prev = newCage;
+        first = newCage;
     } else {
-        Cage *uk = new Cage;
-        uk->light = light;
-        uk->next = first;
-        uk->prev = end;
-        end->next = uk;
-        end = uk;
-        first->prev = uk;
+        Cage* lastCage = first->prev;
+        lastCage->next = newCage;
+        newCage->prev = lastCage;
+        newCage->next = first;
+        first->prev = newCage;
     }
 }
 
 int Train::getLength() {
-    first->light = true;
-    int kvp = 0;
-    Cage *temp = first;
-    int tempn = 0;
-    while (true) {
-        countOp++;
-        tempn++;
-        temp = temp->next;
-        if (temp->light) {
-            int tmp = tempn;
-            temp->light = false;
-            for (int i = tempn; tempn > 0; tempn--, countOp++) {
-                temp = temp->prev;
-            }
-            if (!temp->light) {
-                kvp = tmp;
-                break;
-            }
+    Train::Cage* CurWagon = first;
+    if (!CurWagon->light) {
+        CurWagon->light = true;
+    }
+    int wagCount = 0;
+    int finlen = 0;
+    bool continueCount = true;
+    while (continueCount) {
+        CurWagon = CurWagon->next;
+        countOp += 1;
+        wagCount += 1;
+        while (!CurWagon->light) {
+            CurWagon = CurWagon->next;
+            countOp += 1;
+            wagCount += 1;
+        }
+        CurWagon->light = false;
+        finlen = wagCount;
+        for (wagCount; wagCount > 0; wagCount--) {
+            countOp += 1;
+            CurWagon = CurWagon->prev;
+        }
+        if (CurWagon->light == false) {
+            continueCount = false;
         }
     }
-    return kvp;
+    return finlen;
 }
 
 int Train::getOpCount() {
